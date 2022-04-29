0179381b75655bfcef3326d9283e80dc4831e9a3
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-04-29 14:23:10 -0400

    Bootstrap CDN added to index.html

--- react-webpage/src/Navbar.js
+++ react-webpage/src/Navbar.js
@@ -2,40 +2,43 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg text-white bg-dark">
-      <a class="navbar-brand text-success" href="#">
+    <nav className="navbar navbar-expand-lg text-white bg-dark">
+      <a className="navbar-brand text-success" href="#">
         Navbar
       </a>
 
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link text-white" href="#">
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link text-white" href="#">
               Home{" "}
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="#">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">
               About Us
             </a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link text-white">Products</a>
+          <li className="nav-item dropdown">
+            <a className="nav-link text-white">Products</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
               Meeting
             </a>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
+        <form className="form-inline my-2 my-lg-0">
           <input
-            class="form-control mr-sm-2"
+            className="form-control mr-sm-2"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
             Search
           </button>
         </form>

a1839b426ca7ccee337219319b3b7be562be6bc7
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-04-29 20:50:02 -0400

    Featured Section Component added

--- react-webpage/src/App.js
+++ react-webpage/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Jumbotron from "./Jumbotron";
+import Featured from "./Featured";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Jumbotron />
+      <Featured />
     </div>
   );
 };

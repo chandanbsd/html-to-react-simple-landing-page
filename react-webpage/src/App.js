fae9b6f835325f00876ce019564315d8a2201cb8
chandanbsd <49138440+chandanbsd@users.noreply.github.com>
2022-04-29 20:43:39 -0400

    Jumbotron Component Added

--- react-webpage/src/App.js
+++ react-webpage/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Jumbotron />
     </div>
   );
 };
